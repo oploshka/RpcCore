0b5f45ee613a933826cf64dc56e1da8653742dd5
Andrey Tyurin <ectb08@mail.ru>
2019-07-23 16:07:25 +0300

    V03 stabilized basic multipart json rpc request

--- src/Code/ReturnFormatter/MultipartJsonRpc_v0_1.php
+++ src/Code/ReturnFormatter/MultipartJsonRpc_v0_1.php
@@ -10,6 +10,35 @@ class MultipartJsonRpc_v0_1 implements \Oploshka\RpcInterface\ReturnFormatter{
     $this->Reform = new \Oploshka\Reform\Reform([]);
   }
 
+  public static function getDefaultRequest(){
+    return [
+      'specification'         => 'multipart-json-rpc',
+      'specificationVersion'  => '0.1.0',
+      'version'               => '1.0.0',               // TODO
+      'language'              => 'en',                  // TODO
+
+      'response'              => [
+        'requestId' => null,
+        'error'     => [ 'code' => 'ERROR_DEFAULT_REQUEST' ],
+        'data'      => [],
+      ],
+    ];
+  }
+
+  public function formatSingle($obj) {
+
+    $Response = $obj['responseList'][0];
+    $loadData = $obj['loadData'] ?? null;
+
+    $responseObj = self::getDefaultRequest();
+    $responseObj['response']['requestId'] = isset($loadData['request']['id']) ? $loadData['request']['id'] : null;
+    $responseObj['response']['error']['code'] = $Response->getError();
+    $responseObj['response']['data'] = $Response->getData();
+
+    return $responseObj;
+  }
+
+
   public function format($obj) {
     /*[
       'requestType'   => $requestType,
@@ -18,47 +47,20 @@ class MultipartJsonRpc_v0_1 implements \Oploshka\RpcInterface\ReturnFormatter{
       // 'methodList'   => [],
     ]*/
 
-    /*
-   {
-     "specification": "multipart-json-rpc",
-     "specificationVersion" : "0.1.0",
 
-     "version": "1",
-     "language": "en",
-
-    "response": {
-      "requestId"   : "9423234",
-      "error": {
-        "code": "ERROR_NO"
-      },
-      "data": [ "user": USER_CLASS ]
+    if($obj['requestType'] === 'multiple'){
+      $responseObj = [];
+    } else {
+      $responseObj = $this->formatSingle($obj);
     }
-   }
-   */
-
-
-    $Response = $obj['responseList'][0];
-
-    $responseObj = [
-      'specification'         => 'multipart-json-rpc',  // TODO
-      'specificationVersion'  => '0.1.0',               // TODO
-      'version'               => '1.0.0',               // TODO
-      'language'              => 'en',                  // TODO
-
-      'response'              => [
-        'requestId' => null,
-        'error'     => [ 'code' => $Response->getError() ],
-        'data'      => $Response->getData(),
-      ],
-    ];
 
     $returnJson = $this->Reform->item($responseObj, ['type' => 'objToJson']);
     if($returnJson === NULL){
       $responseObj = [
-        'specification'         => 'multipart-json-rpc',  // TODO
-        'specificationVersion'  => '0.1.0',               // TODO
-        'version'               => '1.0.0',               // TODO
-        'language'              => 'en',                  // TODO
+        'specification'         => 'multipart-json-rpc',
+        'specificationVersion'  => '0.1.0',
+        'version'               => '1.0.0',
+        'language'              => 'en',
 
         'response'              => [
           'requestId' => null,
--- test/Core/CoreCreateTest.php
+++ test/Core/CoreCreateTest.php
@@ -6,7 +6,7 @@ use PHPUnit\Framework\TestCase;
 
 class CoreCreateTest extends TestCase {
 
-  public function testCreate_MultipartJsonRpc_v0_1() {
+  public function test_MultipartJsonRpc_v0_1__BasicRequest() {
 
     // init MultipartJsonRpc_v0_1
     $rpcInitData = [
@@ -30,6 +30,7 @@ class CoreCreateTest extends TestCase {
         "language": "en",
         
         "request" : {
+          "id"   : "testRequestId",
           "name" : "MethodTest1",
           "data" : []
         }
@@ -44,6 +45,54 @@ class CoreCreateTest extends TestCase {
     $this->assertEquals( $returnObj['version']              , '1.0.0');
     $this->assertEquals( $returnObj['language']             , 'en');
     $this->assertEquals( $returnObj['response']['error']['code'], 'ERROR_NO');
+    $this->assertEquals( $returnObj['response']['requestId'], 'testRequestId');
+    $this->assertEquals( $returnObj['response']['data']     , ['test1::string' => 'test string', 'test1::int' => 1]);
   }
 
+
+  public function test_MultipartJsonRpc_v0_1__MultipleRequest() {
+
+    // init MultipartJsonRpc_v0_1
+    $rpcInitData = [
+      'methodStorage'   => \Oploshka\RpcHelperTest\Helper::getRpcMethodStorage()    ,
+      'reform'          => \Oploshka\RpcHelperTest\Helper::getRpcReform()           ,
+      'dataLoader'      => new \Oploshka\RpcDataLoader\PostMultipartFieldJson()     ,
+      'dataFormatter'   => new \Oploshka\RpcDataFormatter\MultipartJsonRpc_v0_1()   ,
+      'returnFormatter' => new \Oploshka\RpcReturnFormatter\MultipartJsonRpc_v0_1() ,
+      'responseClass'   => \Oploshka\RpcHelperTest\Helper::getResponseClass()       ,
+    ];
+    $Rpc = new \Oploshka\Rpc\Core($rpcInitData);
+    $Rpc->applyPhpSettings();
+
+    $_SERVER['REQUEST_METHOD'] = 'POST';
+    $_POST = [
+      'data' => '{
+        "specification": "multipart-json-rpc",
+        "specificationVersion" : "0.1.0",
+        
+        "version": "1",
+        "language": "en",
+        
+        "request" : {
+          "id"   : "testRequestId",
+          "name" : "multiple",
+          "data" : [
+            { "id"   : "testRequestId_2", "name" : "MethodTest1", "data" : [] },
+            { "id"   : "testRequestId_1", "name" : "MethodTest1", "data" : [] }
+          ]
+        }
+      }',
+    ];
+
+    $returnJson = $Rpc->startProcessingRequest();
+    $returnObj = (array) json_decode($returnJson, true);
+
+    $this->assertEquals( $returnObj['specification']        , 'multipart-json-rpc');
+    $this->assertEquals( $returnObj['specificationVersion'] , '0.1.0');
+    $this->assertEquals( $returnObj['version']              , '1.0.0');
+    $this->assertEquals( $returnObj['language']             , 'en');
+    $this->assertEquals( $returnObj['response']['error']['code'], 'ERROR_NO');
+    $this->assertEquals( $returnObj['response']['requestId'], 'asd');
+    $this->assertEquals( $returnObj['response']['data']     , ['test1::string' => 'test string', 'test1::int' => 1]);
+  }
 }
