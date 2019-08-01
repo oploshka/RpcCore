fa2462e2615e36148d130fb0f8e2f068225b2867
Andrey Tyurin <ectb08@mail.ru>
2019-08-01 18:53:07 +0300

    update exception

---
+++ test/Core/MultipartJsonRpcErrorHandlerTest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Oploshka\RpcTest;
+
+use PHPUnit\Framework\TestCase;
+
+class MultipartJsonRpcErrorHandlerTest extends TestCase {
+
+  public function test_MultipartJsonRpc_v0_1__BasicRequest() {
+
+    // init MultipartJsonRpc_v0_1
+    $rpcInitData = [
+      'methodStorage'   => \Oploshka\RpcHelperTest\Helper::getRpcMethodStorage()    ,
+      'reform'          => \Oploshka\RpcHelperTest\Helper::getRpcReform()           ,
+      'dataLoader'      => new \Oploshka\RpcDataLoader\PostMultipartFieldJson()     ,
+      'dataFormatter'   => new \Oploshka\RpcDataFormatter\MultipartJsonRpc_v0_1()   ,
+      'returnFormatter' => new \Oploshka\RpcReturnFormatter\MultipartJsonRpc_v0_1() ,
+      'responseClass'   => \Oploshka\RpcHelperTest\Helper::getResponseClass()       ,
+    ];
+    $Rpc = new \Oploshka\Rpc\Core($rpcInitData);
+    $Rpc->applyPhpSettings();
+
+    $_SERVER['REQUEST_METHOD'] = 'POST';
+    $_POST = [
+      'data' => '{
+        "specification": "multipart-json-rpc",
+        "specificationVersion" : "0.1.0",
+        
+        "version": "1",
+        "language": "en",
+        
+        "request" : {
+          "id"   : "basicRequestId",
+          "name" : "MethodNotValid",
+          "data" : []
+        }
+      }',
+    ];
+
+    $returnJson = $Rpc->startProcessingRequest();
+    $this->assertEquals( $returnJson      , 'TODO');
+  }
+
+}
