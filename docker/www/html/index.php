27415e65a47e21965073c053569fd881d648d369
Andrey Tyurin <ectb08@mail.ru>
2021-12-25 20:01:10 +0300

    update example run struct

---
+++ docker/www/html/index.php
@@ -0,0 +1,17 @@
+<?php
+// Простенький роутер
+
+// берем переданный роут
+$route = trim($_SERVER['REQUEST_URI'] ?? 'index', " \n\r\t\v\0/");
+
+$page = [
+  'form' => true,
+  'api'  => true,
+  'home' => true,
+];
+
+if(!isset($page[$route])){
+  $route = 'home';
+}
+
+include __DIR__. '/../page/'.$route.'.php';
---
+++ docker/www/page/home.php
@@ -0,0 +1,13 @@
+<!DOCTYPE>
+<html>
+<head>
+  <title>Rpc example</title>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+  <link rel="stylesheet" href="/css/style.css">
+</head>
+<body>
+
+<h1>Пример работы rpc</h1>
+
+</body>
+</html>
